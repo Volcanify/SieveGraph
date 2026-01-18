05b4e47c3fbbf2c0e587f39ba95de0d983ac218d
Volcanify <noemail@email>
2026-01-18 21:37:07 +0600

    Updates

--- _app/immutable/chunks/DU1GpwID.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_xumq43?.base??"/sievegraph",e=globalThis.__sveltekit_xumq43?.assets??s??"";export{e as a,s as b};
---
+++ _app/immutable/chunks/fmxeg-p_.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_8siwkc?.base??"/sievegraph",e=globalThis.__sveltekit_8siwkc?.assets??s??"";export{e as a,s as b};
